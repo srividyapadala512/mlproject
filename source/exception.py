b6383b34bec1688cab37774e644c6e25f33864e0
srividyapadala512 <srividyapadala512@gmail.com>
2025-11-14 14:15:37 +0530

    Data Ingestion

--- source/exception.py
+++ source/exception.py
@@ -1,6 +1,7 @@
 import sys
-import logging
-def error_message_detail(error,error_detail: sys):
+from source.logger import logging
+
+def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.b_frame.f_code.co_filename
     error_message=(
@@ -12,15 +13,12 @@ def error_message_detail(error,error_detail: sys):
     return error_message   
     
 class CustomException(Exception):
-    def__init__(self, error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail)
 
     def __str__(self):
         return self.error_message    
 
-
-       
-        
         
     
